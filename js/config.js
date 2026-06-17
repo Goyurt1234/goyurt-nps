448eb128a9f33053d7f18fc54f40877c2fd6af43
Harol Blanco <A.trade@goyurt.com.co>
2026-06-17 15:58:13 -0500

    Actualiza URL Apps Script

--- js/config.js
+++ js/config.js
@@ -1,2 +1,1 @@
-const API_URL =
-"https://script.google.com/a/macros/goyurt.com.co/s/AKfycby4wEAFxjoKAOus1k_hg0AZCFeoO8asmEiPgu8YEtwjrEY05Zqdc832rLjM4XxYsNxh/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby4wEAFxjoKAOus1k_hg0AZCFeoO8asmEiPgu8YEtwjrEY05Zqdc832rLjM4XxYsNxh/exec";
